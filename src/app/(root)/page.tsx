f04d039d3c3e600ceab4b4c089e64ecfc27e44cc
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2023-10-06 11:40:58 -0500

    Text Color

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,8 +4,8 @@ import BulletinBoard from '@/c/global/BulletinBoard';
 
 const Home: React.FC = (): JSX.Element => {
   return (
-    <div className='flex h-full w-full items-center justify-center '>
-      <div className='relative flex h-full w-full max-w-screen-xl text-slate-100'>
+    <div className='flex h-full w-full items-center justify-center'>
+      <div className='relative flex h-full w-full max-w-screen-xl text-slate-200'>
         <LSide />
         <BulletinBoard />
         <RSide />
